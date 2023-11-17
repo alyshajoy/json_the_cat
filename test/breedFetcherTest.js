2096ff3b2d99db2746ab7a74f2a439317fa72621
Alysha Huot <alyshajhuot@gmail.com>
2023-11-17 14:41:58 -0700

    added test files and multiple tests

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,25 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it("returns message to user if searched breed doesn't exist in database", (done) => {
+    fetchBreedDescription("smellycat", (err, desc) => {
+      assert.equal(desc, "We have no data on smellycat.");
+      done();
+    });
+  });
+});
